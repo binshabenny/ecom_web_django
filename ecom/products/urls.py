81a67f661cd0d3427091c5d963fa7f901dfabb67
Binsha Benny <binshabenny3@gmail.com>
2024-02-03 14:57:14 +0530

    product , orders, and customer page layout designs are completed

---
+++ ecom/products/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns = [
+    path('', views.index,name='index_page'),
+    path('products/', views.products,name='products_page'),
+    path('pdt_description/', views.pdt_description, name='description'),
+    path('single_product/', views.single_product, name='single_product'),
+
+
+
+]
--- ecom/products/views.py
+++ ecom/products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def products(request):
+    return render(request,'products.html')
+
+def pdt_description(request):
+    return render(request,'pdt_description.html')
+
+def single_product(request):
+    return render(request,'single_product.html')
+   
+
